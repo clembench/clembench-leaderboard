2d5c3ff72d1c027ab911cd546d5deada1d49eea3
kushal-10 <kushalkoshti11@gmail.com>
2025-03-03 11:15:59 +0100

    update text content

--- src/assets/text_content.py
+++ src/assets/text_content.py
@@ -1,7 +1,7 @@
 TITLE = """<h1 align="center" id="space-title"> 🏆 CLEM Leaderboard</h1>"""
 
-REPO = "https://raw.githubusercontent.com/clembench/clembench-runs/v2.0/"
-REGISTRY_URL = "https://raw.githubusercontent.com/kushal-10/clembench/refs/heads/main/backends/model_registry.json"
+REPO = "https://raw.githubusercontent.com/clembench/clembench-runs/main/"
+REGISTRY_URL = "https://raw.githubusercontent.com/clp-research/clembench/refs/heads/main/backends/model_registry.json"
 BENCHMARK_FILE = "benchmark_runs.json"
 
 HF_REPO = "colab-potsdam/clem-leaderboard"
